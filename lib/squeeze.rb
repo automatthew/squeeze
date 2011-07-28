cb2a93ed4cb3bbd6fb9f41b5f384b14245852af7
Matthew King <mking@borderstylo.com>
2011-07-28 09:29:48 -0700

    we have an instance variable, use it

--- lib/squeeze.rb
+++ lib/squeeze.rb
@@ -13,28 +13,38 @@ class Squeeze
   # Takes an array of hashes, keyed with Symbols
   def reduce(records)
     records.each do |record|
-      process(@output, record, @fields)
+      process(record, @fields)
     end
   end
 
-  def process(output, record, fields, sig=[])
-    fields.each do |field, subfields|
-      key = resolve(field, record)
-      case subfields
-      when true
-        output.increment(*(sig + [field, key]))
-      when Symbol
-        output.increment(*(sig + [field, key, :_count]))
-        value = resolve(subfields, record)
-        output.increment(*(sig + [field, key, subfields, value]))
-      when Array
-        subfields.each do |subfield|
-          process(output, record, {field => subfield}, sig )
-        end
-      when Hash
-        process(output, record, subfields, sig + [field, key])
+  def process(record, fields, sig=[])
+    case fields
+    when Hash
+      fields.each do |field, subfields|
+        process_field(record, field, subfields, sig)
       end
- 
+    when Array
+      fields.each do |field|
+        process_field(record, field, true, sig)
+      end
+    end
+  end
+
+  def process_field(record, field, subfields, sig)
+    key = resolve(field, record)
+    case subfields
+    when true
+      output.increment(sig + [field, key])
+    when Symbol
+      output.increment(sig + [field, key, :_count])
+      value = resolve(subfields, record)
+      output.increment(sig + [field, key, subfields, value])
+    when Array
+      subfields.each do |subfield|
+        process(record, {field => subfield}, sig )
+      end
+    when Hash
+      process(record, subfields, sig + [field, key])
     end
   end
 
